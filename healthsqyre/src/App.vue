45767753d2bbc349e99378cd0bbcd70551f9ece8
Ryan Doner <ryan.doner@techtonic.com>
2020-03-24 12:24:11 -0600

    sort by price and sort by id work

--- healthsqyre/src/App.vue
+++ healthsqyre/src/App.vue
@@ -1,22 +1,51 @@
 <template>
-  <ul>
-    <li v-for="item in items" :key="item.id" >
-      <h1>{{ item.name }}</h1>
-      <h2>{{ item.vendor.name }}</h2>
-      <h2>${{ item.price }}</h2>
-      <h2 v-html="item.description"></h2>
-    </li>
-  </ul>
+  <div>
+    <button v-if=sortedByPrice @click="sortedByPrice = !sortedByPrice">Sort By Id</button>
+    <button v-else @click="sortedByPrice = !sortedByPrice">Sort By Price</button>
+    <!-- <button @click="sorting *= -1">Toggle order</button> -->
+    <ul>
+      <li v-for="item in sortedItems" :key="item.id">
+        <h1>{{ item.id }}</h1>
+        <h1>{{ item.name }}</h1>
+        <h2>{{ item.vendor.name }}</h2>
+        <h2>${{ item.price }}</h2>
+        <h2 v-html="item.description"></h2>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import { DATA } from "../data.const";
 export default {
   name: "App",
-  data() {
-    return {
-      items: DATA
-    };
+  // data() {
+  //   // const sortedById = DATA.sort((a, b) => {
+  //   //     return a.id - b.id;
+  //   //   })
+  //   // const sortedByPrice = DATA.sort((a, b) => {
+  //   //     return a.price - b.price;
+  //   //   })
+  //   return {
+  //     items: DATA
+  //   };
+  // },
+
+  data: () => ({
+    sortedByPrice: false
+  }),
+  computed: {
+    sortedItems() {
+      if (this.sortedByPrice) {
+        return DATA.sort((a, b) => {
+          return a.price - b.price;
+        });
+      } else {
+        return DATA.sort((a, b) => {
+          return a.id - b.id;
+        })
+      }
+    }
   },
 };
 </script>
@@ -30,6 +59,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
-
 </style>
